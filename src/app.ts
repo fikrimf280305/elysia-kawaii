d5821489c19e08436719ddd06b2164c83c4912fa
Fikri Muhammad Fadhilah <179136498+fikrimf280305@users.noreply.github.com>
2024-09-20 13:41:38 +0700

    Add some API Endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Elysia, t } from 'elysia'
+
+import { swagger } from '@elysiajs/swagger'
+
+import { note } from './note'
+
+const app = new Elysia()
+
+    .use(swagger())
+
+    .use(note)    
+
+    .listen(3000)
